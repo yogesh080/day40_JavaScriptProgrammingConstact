ba19b25e816d72c6b4ba6f949caba834365c21a9
Yogesh <yogeshbietjhs@gmail.com>
2022-07-29 13:30:21 +0530

    added Programming construct

--- day40_JavaScriptProgrammingConstact/SequenceProblems/UnitCoversion.js
+++ day40_JavaScriptProgrammingConstact/SequenceProblems/UnitCoversion.js
@@ -4,5 +4,5 @@ console.log("one inch in feet is :"+ft)
 
 const Rectangle = ft*60*40;
 console.log("Recctangle plot in Meter is: " + Rectangle + "mtr")
-
+// area of rechangle
 console.log("Area of 25 such plot is equals to: " + 25*Rectangle + "mtr")
